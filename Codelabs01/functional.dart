88768e04f6c27b2365e3d40ffe18a34271a921f6
Harold Arguello <hjarguellor@gmail.com>
2024-05-25 20:33:12 -0600

    update comments

--- Codelabs01/functional.dart
+++ Codelabs01/functional.dart
@@ -1,14 +1,14 @@
 String scream(int length) => "A${'a' * length}h!"; // retorna la letra a multiplicada por el largo 
 
 main() {
-  final values=[1,2,3,5,10,50];
+  final values=[1,2,3,5,10,50]; //List iterable
   /* for (var length in values) { //iteracion en la que se replica a por cada uno de los valores de Iterable
     print(scream(length));
   } */
-
-  /* values.map(scream).forEach(print); */
+ //        ------------  Encadenamiento de metodos     ------------------------
+  /* values.map(scream).forEach(print);    encadenamiento de metodos*/
   values.skip(1).   // salta el primer elemento
   take(3).          // recorre los siguientes 3 elementos
-  map(scream).      // mapea los elementos concatenando la cantidad de a que se replican
+  map(scream).      // mapea los elementos concatenando la cantidad de "a" que se replican
   forEach(print);   // recorre los elementos del iterable y los envia a pantalla.
 }
--- Codelabs01/shape.dart
+++ Codelabs01/shape.dart
@@ -1,25 +1,25 @@
 import 'dart:math';
 
-abstract class Shape{
+abstract class Shape{   //definicion de clase abstracta
   factory Shape(String type){
   if(type == 'circle')  return Circle(2);
   if(type== 'square')  return Square(2);
   throw 'can\'t create $type.';
   }
-  num get area;
+  num get area;     //metodo de la clase abstracta
 }
 
-class Circle implements Shape{
+class Circle implements Shape{      //crea la clase circle a partir de la clase abstracta Shape
   final num radius;
   Circle(this.radius);
-  num get area => pi * pow(radius,2);
+  num get area => pi * pow(radius,2);   //retorna el area de un circulo
 
 }
 
-class Square implements Shape{
+class Square implements Shape{      //crea la clase circle a partir de la clase abstracta Shape
   final num side;
   Square(this.side);
-  num get area => pow(side, 2);
+  num get area => pow(side, 2);     // retorna el area de un cuadrado
 }
 
 /* Shape shapeFactory(String type) {
@@ -28,7 +28,7 @@ class Square implements Shape{
   throw 'can\'t create $type.';
 } */
 
-class CircleMock implements Circle {
+class CircleMock implements Circle { //implementacion de interfaz en Dart
   num area=0;
   num radius=0;
 }
